5bc21c0321a68c81e792c0c8ea7fd59869bb01c7
hannalvilms <hanna-liisa.vilms@khk.ee>
2020-10-27 23:21:48 +0200

    Salvestatud form'i andmed andmebaasi.

--- src/Controller/BookingIndex.php
+++ src/Controller/BookingIndex.php
@@ -1,27 +1,34 @@
 <?php
 
-
 namespace App\Controller;
 
+use DateTime;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-use Symfony\Component\Form\Extension\Core\Type\DateType;
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TimeType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
-use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
-use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Booking;
 
 class BookingIndex extends AbstractController
 {
     /**
      * @Route("/create_booking")
      */
-    public function create_booking()
+    public function create_booking(Request $request)
     {
         $form = $this->createFormBuilder()
             ->add('firstName', TextType::class, [
@@ -52,13 +59,13 @@ class BookingIndex extends AbstractController
                 'required' => true,
                 'widget' => 'single_text'
             ])
-            ->add('numberOfPeople', IntegerType::class, [
+            ->add('nrOfPeople', IntegerType::class, [
                 'required' => true
             ])
             ->add('payingMethod', ChoiceType::class, [
                 'choices' => [
                     'cash' => 'cash',
-                    'card' => 'card'
+                    'transfer' => 'transfer'
                 ],
                 'required' => true
             ])
@@ -70,13 +77,42 @@ class BookingIndex extends AbstractController
             ])
             ->getForm();
 
+        if($request->isMethod('POST')){
+            $form->submit($request->request->get($form->getName()));
+            if($form->isSubmitted() && $form->isvalid()){
+                $data = $form->getData();
+
+                $entityManager = $this->getDoctrine()->getManager();
+
+                $booking = new Booking();
+
+                $booking->setFirstName($data['firstName']);
+                $booking->setLastName($data['lastName']);
+                $booking->setPhone($data['phone']);
+                $booking->setEmail($data['email']);
+                $booking->setBirthday(new \DateTime($data['birthday']->format('Y-m-d')));
+                $booking->setStartDate(new \DateTime($data['startDate']->format('Y-m-d')));
+                $booking->setEndDate(new \DateTime($data['endDate']->format('Y-m-d')));
+                $booking->setArrivalTime(new \DateTime($data['arrivalTime']->format('H:i:s')));
+                $booking->setnrOfPeople($data['nrOfPeople']);
+                $booking->setPayingMethod($data['payingMethod']);
+                $booking->setAdditionalInformation($data['additionalInformation']);
+
+                $entityManager->persist($booking);
+
+                $entityManager->flush();
+
+                return $this->redirectToRoute('bookings');
+            }
+        }
+
         return $this->render('bookings/create_booking.html.twig', [
             'form' => $form->createView(),
         ]);
     }
 
     /**
-     * @Route("/bookings")
+     * @Route("/bookings", name="bookings")
      */
     public function bookings()
     {
